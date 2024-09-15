8b09794c42eb1b00a95d9d75d09680a5bc661e08
Nabeel <rameezhashmi1234@gmail.com>
2024-09-15 01:52:45 -0700

    Change URL

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://mern-front-nine.vercel.app/',
     credentials: true
 }))
 app.use('/',authRoute)
